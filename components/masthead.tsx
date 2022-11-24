d5780b29ecb169faff7606d3f99a90f3e2b164c8
kapatiuska <65649227+jordicapedo@users.noreply.github.com>
2022-11-24 17:36:59 +0100

    ✨ Develop masthead component

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -1,9 +1,31 @@
-import React from "react";
-import Image from "next/image";
+import React, { useRef, useContext, useState, useCallback } from 'react'
+import Image from 'next/image'
+import { ScrollContext } from '../utils/scroll-observer'
 
 const Masthead: React.FC = () => {
+  const [imageLoaded, setImageLoaded] = useState(false)
+  const refContainer = useRef<HTMLDivElement>(null)
+  const { scrollY } = useContext(ScrollContext)
+
+  let progress = 0
+
+  const { current: elContainer } = refContainer
+  if (elContainer) {
+    progress = Math.min(1, scrollY / elContainer.clientHeight)
+  }
+
+  const handleImageLoaded = useCallback(() => {
+    setImageLoaded(true)
+  }, [])
+
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center -z-10">
+    <div
+      ref={refContainer}
+      className="min-h-screen flex flex-col items-center justify-center sticky top-0 -z-10"
+      style={{
+        transform: `translateY(-${progress * 20}vh)`
+      }}
+    >
       <video
         autoPlay
         muted
@@ -16,7 +38,9 @@ const Masthead: React.FC = () => {
         <source src="/assets/masthead-bg.webm" type="video/webm; codecs=vp9" />
       </video>
       <div
-        className={`flex-grow-0 pt-10 transition-opacity duration-1000 z-10`}
+        className={`flex-grow-0 pt-10 transition-opacity duration-1000 z-10 ${
+          imageLoaded ? 'opacity-100' : 'opacity-0'
+        }`}
       >
         <Image
           src="/assets/logo.svg"
@@ -32,16 +56,21 @@ const Masthead: React.FC = () => {
           <span>done right</span>
         </h2>
       </div>
-      <div className="z-10 flex-grow-0 pb-20 md:pb:10 transition-all duration-1000">
+      <div
+        className={`z-10 flex-grow-0 pb-20 md:pb:10 transition-all duration-1000 ${
+          imageLoaded ? 'opacity-100' : 'opacity-0 -translate-y-10'
+        }`}
+      >
         <Image
           src="/assets/arrow-down.webp"
           width={188 / 3}
           height={105 / 3}
           alt="scroll down"
+          onLoad={handleImageLoaded}
         />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Masthead;
+export default Masthead
