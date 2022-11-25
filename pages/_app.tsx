12f48af65a4fb9f0ab8960dbc9fec84bdd7558c6
kapatiuska <65649227+jordicapedo@users.noreply.github.com>
2022-11-25 13:11:40 +0100

    ✨ wrap size observer util to the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import ScrollObserver from '../utils/scroll-observer'
+import SizeObserver from '../utils/size-observer'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ScrollObserver>
-      <Component {...pageProps} />
-    </ScrollObserver>
+    <SizeObserver>
+      <ScrollObserver>
+        <Component {...pageProps} />
+      </ScrollObserver>
+    </SizeObserver>
   )
 }
